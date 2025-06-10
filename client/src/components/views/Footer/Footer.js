bd8e85a573d34118c4219acf3dec38cd18466016
Chethan Nazre S <chethannazre009@gmail.com>
2025-06-10 20:22:27 +0530

    Initial commit

---
+++ client/src/components/views/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Icon} from 'antd';
+
+function Footer() {
+    return (
+        <div style={{
+            height: '80px', display: 'flex',
+            flexDirection: 'column', alignItems: 'center',
+            justifyContent: 'center', fontSize:'1rem'
+        }}>
+           <p> <h3>©2025 Chethan Nazre S - ALL RIGHTS RESERVED</h3></p>
+        </div>
+    )
+}
+
+export default Footer
